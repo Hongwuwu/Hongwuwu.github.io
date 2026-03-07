5bf720888331899773355c8c2951aa4ee6b6626c
Hongwuwu <3245340787@qq.com>
2026-03-07 16:35:53 +0800

    Site updated: 2026-03-07 16:35:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/08/hello-world/","2026/02/08/testPost/","2026/02/15/hexo/"];function toRandomPost(){
+var posts=["2026/02/08/hello-world/","2026/03/07/测试！这是一条由Claud编辑并发布的Blog！/","2026/02/15/hexo/","2026/02/08/testPost/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
