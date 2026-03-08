741fddc6f90bcc99ff39bb3aa563060a4312fe36
Hongwuwu <3245340787@qq.com>
2026-03-08 21:18:07 +0800

    Site updated: 2026-03-08 21:18:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/07/A-Month-2/","2026/03/08/Vjepa/","2026/02/08/hello-world/","2026/02/15/hexo/","2026/02/08/testPost/","2026/03/07/测试！这是一条由Claude编辑并发布的Blog！/"];function toRandomPost(){
+var posts=["2026/03/08/Vjepa/","2026/03/07/A-Month-2/","2026/02/08/hello-world/","2026/02/08/testPost/","2026/02/15/hexo/","2026/03/07/测试！这是一条由Claude编辑并发布的Blog！/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
