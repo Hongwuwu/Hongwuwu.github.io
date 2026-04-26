afc65a26587cab50fd873aba3231bdb8573382dc
Hongwuwu <3245340787@qq.com>
2026-04-26 16:20:32 +0800

    Site updated: 2026-04-26 16:20:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/08/hello-world/","2026/03/07/A-Month-2/","2026/03/08/Vjepa/","2026/04/26/需求工程-复习笔记/","2026/02/15/hexo/","2026/03/07/测试！这是一条由Claude编辑并发布的Blog！/","2026/02/08/testPost/"];function toRandomPost(){
+var posts=["2026/02/08/hello-world/","2026/03/07/测试！这是一条由Claude编辑并发布的Blog！/","2026/04/26/需求工程-复习笔记/","2026/02/15/hexo/","2026/03/08/Vjepa/","2026/02/08/testPost/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
