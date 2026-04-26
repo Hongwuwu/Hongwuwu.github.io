95e0fb41cd126b689ae3da501a66a380420d45c1
Hongwuwu <3245340787@qq.com>
2026-04-26 17:01:34 +0800

    Site updated: 2026-04-26 17:01:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/08/hello-world/","2026/03/07/测试！这是一条由Claude编辑并发布的Blog！/","2026/04/26/需求工程-复习笔记/","2026/02/15/hexo/","2026/03/08/Vjepa/","2026/02/08/testPost/"];function toRandomPost(){
+var posts=["2026/02/15/hexo/","2026/03/08/Vjepa/","2026/04/26/irweb-工业巡检智能助手部署手册/","2026/02/08/testPost/","2026/04/26/需求工程-复习笔记/","2026/03/07/测试！这是一条由Claude编辑并发布的Blog！/","2026/02/08/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
